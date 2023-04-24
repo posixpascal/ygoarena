5b3dabd0b2bf2602b8e34161099770ff3b588bc0
Pascal <posixpascal@googlemail.com>
2023-04-24 20:09:11 +0200

    add env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       repo : 'git@github.com:posixpascal/ygoarena.git',
       path : '/opt/ygoarena/',
       'pre-deploy-local': '',
-      'post-deploy' : 'rm /opt/ygoarena/source/.env && ln -s shared/.env /opt/ygoarena/source/.env && pnpm install && pnpm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'rm /opt/ygoarena/source/.env && ln -sf /opt/ygoarena/shared/.env /opt/ygoarena/source/.env && pnpm install && pnpm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
